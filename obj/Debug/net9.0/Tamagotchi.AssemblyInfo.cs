50bcc8dd1b247da1a9882538192f8b603f00e680
Yoon Lopez <yoon.lopez.7e9@itb.cat>
2026-03-08 16:02:26 +0100

    Added readme

--- obj/Debug/net9.0/Tamagotchi.AssemblyInfo.cs
+++ obj/Debug/net9.0/Tamagotchi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03acae34a00f3f03cf9db3fc05a00bbd27cfed10")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
